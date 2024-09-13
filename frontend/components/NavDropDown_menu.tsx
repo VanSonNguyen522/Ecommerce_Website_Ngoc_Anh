dcf723ee5c20df7713e521cb21e98bb45fcb374c
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-09-13 22:44:49 +0700

    drop_down_menu and istall shacdn

---
+++ frontend/components/NavDropDown_menu.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+
+const NavDropDown_menu = () => {
+  return (
+    <div>
+        <DropdownMenu>
+        <DropdownMenuTrigger><Image className="mx-auto items-center" src='assets/icons/menu.svg' width = {32} height={32} alt="menu"/></DropdownMenuTrigger>
+        <DropdownMenuContent  className='flex flex-col w-[150px] h-auto text-2xl items-center'>
+        <DropdownMenuLabel>Menu</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Home</DropdownMenuItem>
+        <DropdownMenuItem>Product</DropdownMenuItem>
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>About</DropdownMenuItem>
+        <DropdownMenuItem>Contact</DropdownMenuItem>
+        <DropdownMenuItem>Favorite</DropdownMenuItem>
+        </DropdownMenuContent>
+        </DropdownMenu>
+    </div>
+  )
+}
+
+export default NavDropDown_menu
