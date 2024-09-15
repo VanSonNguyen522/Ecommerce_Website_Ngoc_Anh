b2a95ed7815ba29572745f650e634c769beca686
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-09-15 11:56:37 +0700

    setup route for SigninPage and RegisterPage

---
+++ frontend/app/(root)/Loginpage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LoginPage() 
+{
+    return (
+        <main>
+            <div>Login Page</div>
+            <div>
+                Logout
+            </div>
+        </main>
+    )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
+import LoginPage from "./(root)/Loginpage";
 import HomePage from "./Feature/HomePage/HomePage";
 
 const App: React.FC = () => {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <LoginPage />
     </div>
   );
 };
